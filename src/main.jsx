164ed7eacad9f2d442a97db2a247e71203c5e612
vsamopyt <v.samopyt14@gmail.com>
2024-11-10 11:06:23 +0100

    set redux favourites to localStoraages

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
-import { Provider} from 'react-redux';
-import "modern-normalize";
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client';
+// import {BrowserRouter} from "react-router-dom";
+// import { Provider} from 'react-redux';
+// import "modern-normalize";
+// import App from './components/App/App.jsx';
+// import {store} from "./redux/store.js"
+// import './index.css'
 
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//     <Provider store={store}>
+//     <App />
+//     </Provider>
+//     </BrowserRouter>
+//   </StrictMode>
+// )
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
+import 'modern-normalize';
 import App from './components/App/App.jsx';
-import {store} from "./redux/store.js"
-import './index.css'
+import { store } from './redux/store.js';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-    <App />
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
-)
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,54 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import campersReducer from "./campers/slice.js"
+// import filtersReducer from "./filters/slice.js"
+// import favouritesReducer from "./favourites/slice.js"
+
+// export const store =configureStore({
+// reducer: {
+//     campers: campersReducer,
+//     filters: filtersReducer,
+//     favourites: favouritesReducer,
+// }
+
+// });
+
 import { configureStore } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from "redux-persist";
+import storage from "redux-persist/lib/storage"; 
+
 import campersReducer from "./campers/slice.js"
 import filtersReducer from "./filters/slice.js"
 import favouritesReducer from "./favourites/slice.js"
+
+const persistConfig = {
+    key: "favourites",
+    storage,
+  };
+
+  const persistedFavouriteReducer = persistReducer(persistConfig, favouritesReducer);
 
 export const store =configureStore({
 reducer: {
     campers: campersReducer,
     filters: filtersReducer,
-    favourites: favouritesReducer,
-}
+    favourites: persistedFavouriteReducer,
+}, 
+middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 
 });
+
+export const persistor = persistStore(store);
