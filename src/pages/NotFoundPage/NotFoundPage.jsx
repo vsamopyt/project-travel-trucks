88380387ca56264bd8d873db75b455d04d01f370
vsamopyt <v.samopyt14@gmail.com>
2024-11-15 18:37:27 +0100

    added animation to interactive elements

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,22 +1,20 @@
-import {Link} from 'react-router-dom';
-import clsx from "clsx";
-import css from "./NotFoundPage.module.css"
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import css from './NotFoundPage.module.css';
 const NotFoundPage = () => {
   return (
-  
-      <section className="section">
-        <div className={clsx("container", css.notFoundPageContainer)}>
+    <section className="section">
+      <div className={clsx('container', css.notFoundPageContainer)}>
         <span className={css.notFoundPageError}>404</span>
         <h1 className={css.pageNotFoundHeadline}>page not found</h1>
         <div className={css.notFoundPageTextContainer}>
-        <p>take me back to</p>
-        <Link className={ css.notFoundPageLink}to ="/">Home</Link>
+          <p>take me back to</p>
+          <Link className={css.notFoundPageLink} to="/">
+            Home
+          </Link>
         </div>
-        </div>
-
-     
-      </section>
- 
+      </div>
+    </section>
   );
 };
 export default NotFoundPage;
